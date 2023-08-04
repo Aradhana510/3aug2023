50912077ef3ec65055685b377cccb2407b878223
Aradhana singh <66427825+Aradhana510@users.noreply.github.com>
2023-08-03 19:45:47 -0700

    Add files via upload

---
+++ Assessment22/Program.cs
@@ -0,0 +1,38 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace Assignment22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create instances of Source and Destination classes
+            Source source = new Source
+            {
+                Id = 420,
+                Name = "Veenu Joon",
+                Age = 25,
+                Address = "Jharsa gurgaon",
+                City="gurgaon"
+            };
+
+            Destination destination = new Destination();
+
+            // Map properties from the source to destination
+            PropertyMapper.MapProperties(source, destination);
+
+            // Display the values of properties in the Destination class to ensure the mapping was successful
+            Console.WriteLine($"Destination Id: {destination.Id}");
+            Console.WriteLine($"Destination Name: {destination.Name}");
+            Console.WriteLine($"Destination Age: {destination.Age}");
+            Console.WriteLine($"Destination City: {destination.City}"); // Additional property in Destination class
+            Console.ReadKey();
+        }
+    }
+    
+}
